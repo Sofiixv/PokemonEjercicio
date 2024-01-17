3e7c638184b6a307cbecb07e66bb04148d12f798
Sofi <121338623+Sofiixv@users.noreply.github.com>
2024-01-17 20:39:46 +0100

    Add files via upload
    PokemonV2

---
+++ Pokemon2/src/es/daw/utils/Utils.java
@@ -0,0 +1,15 @@
+package es.daw.utils;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Utils {
+   
+    public static void pintarMenu() {
+        System.out.println("Que desea hacer?");
+        System.out.println("----------------------------------");
+        System.out.println("    1.Añadir Pokemons (Se añaden 2)\n    2.Empezar el combate\n    3.Ver la Pokedex\n    4.Salir");
+
+    }    
+    
+}
